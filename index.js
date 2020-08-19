b8e8dc63c6dc14561d625ddc88f69ae808c8a4a1
MichaelC1999 <69168498+MichaelC1999@users.noreply.github.com>
2020-08-18 20:25:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const searchUrl = 'https://developer.nps.gov/api/v1/parks?statecode='
+const searchUrl = 'https://developer.nps.gov/api/v1/parks?'
 const apiKey= 'zE96KJfgMk1ygjwhNzWLWBYyRCkeCDQqq0BG6Exa'
 
 function getParks(state, maxResult){
   
-  let totalUrl = searchUrl + state + '&api_key='+ apiKey + "&limit=" + maxResult
+  let stateCodeGen = []
+
+  for(let i = 0; i<state.length; ++i){
+    stateCodeGen.push('statecode=' + state[i])
+  }
+
+  stateCodeGen = stateCodeGen.join('&')
+
+  console.log(stateCodeGen)
+
+  let totalUrl = searchUrl + stateCodeGen + '&api_key='+ apiKey + "&limit=" + maxResult
   console.log(totalUrl)
   fetch(totalUrl)
     .then(response => {
@@ -23,9 +33,16 @@ function getParks(state, maxResult){
 function displayResults(responseJson){
   //console.log(responseJson)
   $('#results-list').html("")
+
+  
+
+  if(responseJson.data.length<1){
+    $('#error-message').text('No parks found. Maybe your abreviation was wrong?')
+  }
+
   for(let i = 0; i<responseJson.data.length; ++i){
-    console.log(responseJson.data[i].images)
-    $('#results-list').append('<li><a href="' +responseJson.data[i].url + '">'+responseJson.data[i].name+'</a><img src="'+responseJson.data[i].images[0].url+'"><p>'+responseJson.data[i].description+'</p></li>')
+    
+    $('#results-list').append('<li><a href="' +responseJson.data[i].url + '">'+responseJson.data[i].fullName+'</a><img src="'+responseJson.data[i].images[0].url+'"><p>'+responseJson.data[i].description+'</p></li>')
   }
 
   
@@ -36,10 +53,16 @@ function submit() {
   $('form').submit(event => {
     event.preventDefault();
     
-    const state = $('#search-state').val();
+    $('#error-message').text('')
+
+    let state = $('#search-state').val();
+    state= state.split(", ")
+
     const maxResult = $('#max-num').val();
-    console.log('started')
-   
+    console.log('started ' + state.length)
+
+    $('#results-list').html("<h4>Search started. Loading results now.</h4>")
+
     getParks(state, maxResult);
   });
 }
